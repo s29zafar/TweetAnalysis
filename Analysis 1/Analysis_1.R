40532ca7e07b8a3a2c610767ecd60ca24f5cff11
Saim Zafar <69566994+s29zafar@users.noreply.github.com>
2023-05-08 10:40:40 -0400

    Update Analysis_1.R

--- Analysis 1/Analysis_1.R
+++ Analysis 1/Analysis_1.R
@@ -4,7 +4,7 @@ mydata <- read_csv("Desktop/Stat 231/A1/stat231dataset20888160.csv")
 # Selecting the personal and organizational accounts [Q1(a)]
 user1 <- subset(mydata, subset = (username == "@theJagmeetSingh"))
 user2 <- subset(mydata, subset = (username == "@UWaterloo"))
-# Q1(b)
+
 # Table for Individual
 table(factor(user1$day.of.week, levels = 
 c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")))
@@ -12,7 +12,7 @@ c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")))
 # Table for Organization
 table(factor(user2$day.of.week, levels = 
                c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")))
-# Q1(c)
+
 # plot for Individual
 barplot(table(factor(user1$day.of.week, levels = 
     c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))),
@@ -25,7 +25,7 @@ barplot(table(factor(user2$day.of.week, levels =
         xlab = "Days of the Week", ylab = "Frequency", las = 1, 
         main = "Bar plot showing the number of tweets per day - Organization Account",
         col = "dodgerblue3", ylim = c(0,50))
-# Starting Q2b
+
 # For this question we have to alter the initial mydata data set
 mydata$time.of.day.hour <- mydata$time.of.day/3600
 # We then make new subsets into user1 and user2
@@ -45,7 +45,7 @@ kurtosis(user1$time.of.day.hour)
 # For Organization
 skewness(user2$time.of.day.hour)
 kurtosis(user2$time.of.day.hour)
-# Q2 (C)
+
 # Save Sd and Mean
 time.of.day.hour.mean.user1 <- mean(user1$time.of.day.hour)
 time.of.day.hour.sd.user1 <- sd(user1$time.of.day.hour)
@@ -62,7 +62,7 @@ truehist(user2$time.of.day.hour, xlab = "time of day(hour)",
          ylab = "Relative Frequency",main = "Relative Frequency of Organization User",
          ylim = c(0, 0.25), xlim = c(0,25) ,las = 1, col = "dodgerblue3", density = 25, angle = 45)
 curve(dnorm(x, time.of.day.hour.mean.user2, time.of.day.hour.sd.user2), col = "red", add = TRUE, lwd = 2)
-# Q2 (D) for user1
+#  for user1
 plot(ecdf(user1$time.of.day.hour), xlab = "time of day hour",
      main = "e.c.d.f. of time of day hour for Individual User",
      las = 1, lwd = 2, pch = NA)
@@ -72,11 +72,11 @@ plot(ecdf(user2$time.of.day.hour), xlab = "time of day hour",
      main = "e.c.d.f. of time of day hour for Organization User",
      las = 1, lwd = 2, pch = NA)
 curve(pnorm(x, time.of.day.hour.mean.user2, time.of.day.hour.sd.user2), col = "red", add = TRUE, lwd = 2)
-# For Q2(E) for User 1
+# for User 1
 quantile(user1$time.of.day.hour, probs = 0.1)
 # for User 2
 quantile(user2$time.of.day.hour, probs = 0.1)
-# Start of Q3(b)
+
 user2$likes.log <- log(user2$likes + 1)
 boxplot(user2$likes.log[user2$time.of.day.hour < 12],
         user2$likes.log[user2$time.of.day.hour >= 12],
@@ -84,14 +84,14 @@ boxplot(user2$likes.log[user2$time.of.day.hour < 12],
         xlab = "Time of day hour", ylab = "Log value of Likes",
         names = c("Before Noon", "After Noon"),
         main = "@UWaterloo's Likes Log and Time of the Day")
-# Q3(c)
+
 boxplot(user2$likes.log[user2$time.of.day.hour < 12],
         user2$likes.log[user2$time.of.day.hour >= 12],
         col = c("dodgerblue1", "darkorchid4"),
         xlab = "Time of day hour", ylab = "Log value of Likes",
         names = c("Before Noon", "After Noon"),
         main = "@UWaterloo's Likes Log and Time of the Day")
-# Q3(d)
+
 boxplot(user2$likes.log[user2$day.of.week == "Monday" | user2$day.of.week == "Tuesday" |
                         user2$day.of.week == "Wednesday" | user2$day.of.week == "Thursday" |
                         user2$day.of.week == "Friday"],
